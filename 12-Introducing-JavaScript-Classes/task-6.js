3e30bab03ece697cac423f2d52ca704976ae424c
onsu <jeiranashvili@gmail.com>
2019-06-15 20:54:20 +0400

    feat(12-Introducing-Javascript-Classes): Finish task-6

--- 12-Introducing-JavaScript-Classes/task-6.js
+++ 12-Introducing-JavaScript-Classes/task-6.js
@@ -1,9 +1,30 @@
+class User {
+	constructor(name, surname) {
+		this.name = name;
+		this.surname = surname;
+	}
 
+	getFullName() {
+		return this.name + ' ' + this.surname;
+	}
+}
+
+class Student extends User {
+    constructor(name, surname, year) {
+        super(name, surname);
+        this.year = year;
+    }
+
+    getCourse() {
+        const curryear = new Date().getFullYear();
+        return curryear - this.year;
+    }
+}
 
 var student = new Student('John', 'Smith', 2015);
 
 console.log(student.name); // print 'John'
-console.log(student.surname); // print 'John'
+console.log(student.surname); // print 'John' < actually Smith
 console.log(student.getFullName()); // print 'John Smith'
 console.log(student.year); // print 2015
-console.log(worker.getCourse()); // print 4 - fourth course, because current year 2019
+console.log(student.getCourse()); // print 4 - fourth course, because current year 2019
